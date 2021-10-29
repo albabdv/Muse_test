fbb9be838669e4e12cc1c345489ad5de2d207d44
alba <albabdv@gmail.com>
2021-10-29 13:03:57 +0200

    Add project files.

---
+++ Muse_test/Muse_test.f90
@@ -0,0 +1,25 @@
+!  Muse_test.f90 
+!
+!  FUNCTIONS:
+!  Muse_test - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: Muse_test
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program Muse_test
+
+    implicit none
+
+    ! Variables
+
+    ! Body of Muse_test
+    print *, 'Hello World'
+
+    end program Muse_test
+
